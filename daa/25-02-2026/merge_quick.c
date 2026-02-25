bf51e190cbe0f22fe3a3d3e90863d89c47aefe25
Rhel <triedbutrefused@gmail.com>
2026-02-25 06:25:56 +0530

    initial commit

---
+++ daa/25-02-2026/merge_quick.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+
+void merge(int arr[], int left, int mid, int right)
+{
+    int i, j, k;
+    int leftSize = mid - left + 1;
+    int rightSize = right - mid;
+    int leftArr[leftSize], rightArr[rightSize];
+    for (i = 0; i < leftSize; i++)
+        leftArr[i] = arr[left + i];
+    for (j = 0; j < rightSize; j++)
+        rightArr[j] = arr[mid + 1 + j];
+    i = 0;
+    j = 0;
+    k = left;
+    while (i < leftSize && j < rightSize)
+    {
+        if (leftArr[i] <= rightArr[j])
+            arr[k++] = leftArr[i++];
+        else
+            arr[k++] = rightArr[j++];
+    }
+    while (i < leftSize)
+        arr[k++] = leftArr[i++];
+    while (j < rightSize)
+        arr[k++] = rightArr[j++];
+}
+
+void mergeSort(int arr[], int left, int right)
+{
+    if (left < right)
+    {
+        int mid = (left + right) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = low - 1;
+    int temp;
+    for (int j = low; j < high; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void inputArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+}
+
+void displayArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main()
+{
+    int n, choice;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    inputArray(arr, n);
+    do
+    {
+        printf("\nMenu\n");
+        printf("1. Quick Sort\n");
+        printf("2. Merge Sort\n");
+        printf("3. Exit\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+        if (choice == 1)
+        {
+            quickSort(arr, 0, n - 1);
+            printf("Sorted array using Quick Sort:\n");
+            displayArray(arr, n);
+        }
+        else if (choice == 2)
+        {
+            mergeSort(arr, 0, n - 1);
+            printf("Sorted array using Merge Sort:\n");
+            displayArray(arr, n);
+        }
+    } while (choice != 3);
+    return 0;
+}
