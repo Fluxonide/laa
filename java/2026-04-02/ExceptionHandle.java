0c5ed57343fbef639a79f35129913abfedf293a3
ranit <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>
2026-04-02 12:25:38 +0530

    updated ExceptionHandle.java

--- java/2026-04-02/ExceptionHandle.java
+++ java/2026-04-02/ExceptionHandle.java
@@ -7,39 +7,54 @@ ii. When user try to deposit or withdraw negative amount form their account.
 import java.util.Scanner;
 
 class MyException extends Exception {
-    MyException(String s) { super(s); }
+    MyException(String s) {super(s); }
 }
 
 class Bank {
-    double bal = 1000;
+    private double bal = 1000;
 
     void withdraw(double a) throws MyException {
-        if (a < 0) throw new MyException("neg amount");
-        if (a > 500) throw new MyException("limit cross");
-        if (a > bal) throw new MyException("low balance");
+        if (a < 0) throw new MyException("Negative amount");
+        if (a > 500) throw new MyException("Limit exceeded");
+        if (a > bal) throw new MyException("Insufficient balance");
+
         bal -= a;
-        System.out.println("withdrawn: " + a);
+        System.out.println("Withdrawn: " + a);
     }
 
     void deposit(double a) throws MyException {
-        if (a < 0) throw new MyException("neg amount");
+        if (a < 0) throw new MyException("Negative amount");
+
         bal += a;
-        System.out.println("deposited: " + a);
+        System.out.println("Deposited: " + a);
     }
+
+    double getBalance() {return bal; }
 }
 
-class ExceptionHandle {
+public class ExceptionHandle {
     public static void main(String[] args) {
+        ExceptionHandle obj = new ExceptionHandle(); 
+        obj.run(); 
+    }
+
+    void run() {
         Scanner sc = new Scanner(System.in);
         Bank b = new Bank();
+
         try {
             System.out.print("Enter amount to withdraw: ");
-            b.withdraw(sc.nextDouble());
+            double w = sc.nextDouble();
+            b.withdraw(w);
+
             System.out.print("Enter amount to deposit: ");
-            b.deposit(sc.nextDouble());
+            double d = sc.nextDouble();
+            b.deposit(d);
+
         } catch (MyException e) {
             System.out.println("Error: " + e.getMessage());
         }
-        System.out.println("Balance: " + b.bal);
+
+        System.out.println("Balance: " + b.getBalance());
     }
 }
